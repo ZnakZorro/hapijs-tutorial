26e687e720f8fc928c2ccbaa1a04534be06cdef7
Michal Miszczyszyn <michal@mmiszy.pl>
2017-01-25 00:18:08 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: 3000
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler(request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log(`Server running at ${server.info.uri}`);
+});
